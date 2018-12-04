48f40bfb152f231cabf15c03b68888c9ff0ee85a
Tarik <tarikh@uw.edu>
2018-12-03 16:25:47 -0800

    Created Test package to get repo primed

---
+++ Test/src/test_package/solution.java
@@ -0,0 +1,14 @@
+package test_package;
+
+public class solution {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		int result = giveNumber(3);
+		System.out.println(result);
+	}
+	
+	public static int giveNumber(int num) {
+		return num+2;
+	}
+}
